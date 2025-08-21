ad93b9ae4e6a1df28890f8c0c0b4f9f31e5d562d
mpalssonur <mpalssonur@gmail.com>
2025-08-21 12:23:50 +0000

    update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+conda activate IDNN
+conda update -y --all
+
+rm -rf introduction-to-deep-learning-slides
+git clone https://github.com/Haffi112/introduction-to-deep-learning-slides.git
+rm -rf introduction-to-deep-learning-slides/.git
